1600198e45a7d5638e81f743d56fa87c016e65c0
yossan1208 <yossan431208@gmail.com>
2025-09-29 21:40:30 +0900

    TestDBの作成

---
+++ src/CheckLoadJDBC.java
@@ -0,0 +1,15 @@
+
+public class CheckLoadJDBC {
+
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException  {
+		String msg = "";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			msg = "ドライバのロードに成功しました。";
+		} catch(ClassNotFoundException e) {
+			msg = "ドライバのロードに失敗しました。";
+		}
+		System.out.println(msg);
+	}
+}
