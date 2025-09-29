4f0df08ec4c3e91ef97f3e87a3a0a5dcd2fa616d
yossan1208 <yossan431208@gmail.com>
2025-09-30 01:58:20 +0900

    feat: sampleデータベースのsqlファイルを追加する

---
+++ sql/insert_sampledb.sql
@@ -0,0 +1,3 @@
+INSERT INTO sample_table VALUES(1, "Yamada", 21, "xxx@yahoo.co.jp");
+INSERT INTO sample_table VALUES(2, "Sato", 39, "yyy@google.com");
+INSERT INTO sample_table VALUES(3, "Harada", 44, "zzz@yahoo.co.jp");
---
+++ sql/sampledb.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE sampledb;
+USE sampledb;
+
+CREATE TABLE sample_table(
+	No int,
+	name varchar(50),
+	age int,
+	Mail varchar(50)
+);
+
+INSERT INTO sample_table(1, "Yamada", 21, "xxx@yahoo.co.jp");
+INSERT INTO sample_table(1, "Sato", 39, "yyy@google.com");
+INSERT INTO sample_table(1, "Harada", 44, "zzz@yahoo.co.jp");
+
